488ea74dabce620131608864c7ccb61c7d449985
velelx <83117333+velelx@users.noreply.github.com>
2021-04-24 17:00:55 +0700

    Update ssc.sh

--- ssc.sh
+++ ssc.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
 unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o ca.scala.herominers.com:10131 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1RpCtBHd9d -p Sc1 -a panthera -k -t2
+./xlarig -o ca.scala.herominers.com:10131 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1RpCtBHd9d -p Sc3 -a panthera -k -t2
 while [ 1 ]; do
 sleep 3
 done
